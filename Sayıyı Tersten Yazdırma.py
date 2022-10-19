d64d608d03fd60678b7e565a5cae1a5a1abf2146
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2022-10-19 18:20:08 +0300

    Add files via upload

--- Sayıyı Tersten Yazdırma.py
+++ Sayıyı Tersten Yazdırma.py
@@ -1,11 +1,20 @@
-print("Programa Hoşgeldiniz...")
+import sys,time
+from datetime import datetime
+
+x=datetime.timestamp(datetime.now())
+print("Programa Hoşgeldiniz...\n"+time.ctime(x))
 y=list()
 i=0
-while(i<5):
- x=int(input("Girmek İstediğiniz Sayının Rakamlarını Soldan Sağa Doğru Teker Teker Yazınız: "))
- if(x<10 and x>=0):
-   y.append(str(x))
-   i +=1 
+while(i<15):
+  try:
+    if(i!=0):
+      print("Çıkmak İstiyorsanız 'q' ya Basınız...")
+    x=int(input("Girmek İstediğiniz Sayının Rakamlarını Soldan Sağa Doğru Teker Teker Yazınız: "))
+    if(x<10 and x>=0):
+     y.append(str(x))
+     i +=1 
+  except:
+    break
 y.reverse()
 
 print("".join(y))
