fd296c71ae969be7af0e21118019ddec2ab16a72
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2022-11-20 21:33:24 +0300

    Add files via upload

--- Qrcode Module.py
+++ Qrcode Module.py
@@ -1,5 +1,11 @@
 import qrcode,os
 os.chdir("C:\\Users\\asus\\Desktop\\")
-image=qrcode.make("https://github.com/ataberkkoroglu")
+qr = qrcode.QRCode(version = 1,
+                   box_size = 50,
+                   border = 3)
+data="https://github.com/ataberkkoroglu"
+qr.add_data(data)
+qr.make(fit=True)
+image=qr.make_image(fill_color="black",back_color="blue")
 image.save('Qrcode.png')
 
