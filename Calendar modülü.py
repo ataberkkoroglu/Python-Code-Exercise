5dcd23adad5ea20bcae5ea83a9b9c1e27d9cfa37
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2022-11-21 12:49:50 +0300

    Add files via upload

--- Calendar modülü.py
+++ Calendar modülü.py
@@ -1,25 +1,29 @@
 import calendar,time,sys
 from datetime import datetime
-print("Programa Hoşgeldiniz...\n "+time.ctime(datetime.timestamp(datetime.now())))
+
+print("Welcome To Program...\n "+time.ctime(datetime.timestamp(datetime.now())))
 while(1):
- year=int(input("Yılı Giriniz: "))
- month=input("Ayı Yazınız: ")
+ month=input("Write Month: ")
+ year=int(input("Write year: "))
+ 
  month=month.capitalize()
  print(month)
- x={"Ocak":1 ,"Şubat":2,"Mart":3,"Nisan":4,"Mayıs":5,"Haziran" :6,"Temmuz":7,"Ağustos":8,"Eylül":9,"Ekim":10, "Kasım":11,"Aralık":12}
+ x={"January":1 ,"February":2,"March":3,"April":4,"May":5,"June" :6,"July":7,"August":8,"September":9,"October":10, "November":11,"December":12}
+ y=(1,2)
+ print(type(y))
  for i,e in x.items():
     if i==month:
         print(calendar.month(year,e))
  while(1):
-  sec=input("Devam Etmek İstiyor Musunuz(E/H) ? : ")
-  if (sec=='E' or sec=='e'):
+  sec=input("Would You Like To Contunie(Y/N) ? : ")
+  if (sec=='Y' or sec=='y'):
     time.sleep(2)
     break
-  elif (sec=='H' or sec=='h'):
-    print("Yine Bekleriz...")
+  elif (sec=='N' or sec=='n'):
+    print("Have a Good Days...")
     sys.exit()
   else:
-    sys.__stderr__.write("\nYanlış Girdiniz...\a")
+    sys.__stderr__.write("\nWrong Input...\a")
     sys.__stderr__.flush()
 
 
