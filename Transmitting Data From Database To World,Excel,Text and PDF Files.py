2ae44c57160824f3ff7f601eafc37f8e9bd6bdb4
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-01-29 18:41:00 +0300

    Add files via upload

---
+++ Transmitting Data From Database To World,Excel,Text and PDF Files.py
@@ -0,0 +1,21 @@
+import pandas as pd,os,docx
+from sqlalchemy import create_engine
+from docx2pdf import convert
+con=create_engine('sqlite:///Ogrenci.db').connect()
+
+data=pd.read_sql_table("Ogrenci",con)
+os.chdir("C:/Users/asus/Desktop")
+with pd.ExcelWriter("Ogrenci.xlsx",engine='openpyxl',mode='w') as writer:
+    data.to_excel(writer,'Ogrenci')
+    
+with open("Ogrenci.txt",mode='w',encoding='utf-8') as fp:
+    fp.write(pd.DataFrame.to_string(data))
+    
+
+world=docx.Document()
+world.add_heading("Ogrenci Listesi",5)
+world.add_paragraph(pd.DataFrame.to_string(data))
+world.save("Ogrenci.docx")
+convert("Ogrenci.docx","OgrenciWorld.pdf")
+
+
