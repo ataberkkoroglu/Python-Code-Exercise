c608133c9bfa3ed1ec13bb9befad3cc13f8802e3
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-02-24 12:28:14 +0300

    Update Face.py

--- Face.py
+++ Face.py
@@ -11,8 +11,7 @@ while (1):
         x1,y1,w,h=Mask.ravel()
         cv2.rectangle(frame,(x1,y1),(x1+w,y1+h),(255,0,255),2)
         cv2.putText(frame,"Person",(x1,y1),fontFace=cv2.FONT_HERSHEY_COMPLEX,fontScale=1,color=(255,0,0),thickness=2,lineType=cv2.LINE_AA)
-    """M=cv2.getRotationMatrix2D((frame.shape[0]/1.5,frame.shape[1]/1.5),90,1)
-    frame=cv2.warpAffine(frame,M,(frame.shape[0],frame.shape[1]))"""
+
     frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     thresh=cv2.adaptiveThreshold(frame,127,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     cv2.imshow("Frame",thresh)
