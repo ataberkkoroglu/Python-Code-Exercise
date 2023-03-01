29d5cfb580cfce16e0dfb9556621b4db87b59733
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-03-01 17:38:34 +0300

    Add files via upload

---
+++ Sending message via WhatsApp.py
@@ -0,0 +1,27 @@
+from tkinter import Entry,Button,Label,Tk
+import pywhatkit,pyautogui as py
+from datetime import datetime
+
+def Send():
+ pywhatkit.sendwhatmsg(number.get(),message.get(),time_hour=int(datetime.strftime(datetime.now(),"%H")),time_min=int(datetime.strftime(datetime.now(),"%M"))+1,wait_time=10) 
+ py.click()
+ 
+app=Tk()
+app.config(background="Black")
+app.resizable(1,1)
+app.geometry("640x480")
+app.title("Message")
+text2=Label(app,text="Write Number With Country Code",font=("Arial",24,'bold'),foreground="white",background="black")
+text2.grid(row=3,column=10)
+text=Label(app,text="Write Text",font=("Arial",24,"bold"),foreground="white",background="black")
+text.grid(row=7,column=10)
+#messagebox.showinfo("Information","Information for user")
+number=Entry(app,font=("Arial",24,'bold'),background="White")
+number.grid(row=5,column=10)
+message =Entry(app,font=('Arial',24,'bold'),background="White")
+message.grid(row=9,column=10)
+print(message.get())
+button=Button(app,text="Send Your Message",font=('Arial',11,'normal'),command=Send)
+button.grid(row=11,column=10)
+app.wm_maxsize(630,250)
+app.mainloop()
