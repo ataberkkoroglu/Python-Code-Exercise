838566042112f9107b1ee2ab521ed6e1639b36fc
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-03-09 13:37:06 +0300

    Add files via upload

--- Voice Recorder.py
+++ Voice Recorder.py
@@ -1,14 +1,41 @@
-import sounddevice,colorama.ansi
+import sounddevice,colorama.ansi,sys,time
 from scipy.io.wavfile import write
 from playsound import playsound
-
+import colorama,fontstyle
+from datetime import datetime
 
 fs=44_100 #Sample Rate
-second=float(input("Enter the time Duration in second: ")) #Enter your required time
-print(colorama.ansi.Fore.LIGHTRED_EX,"Recording...\n")
-record_voice=sounddevice.rec(int(second*fs),fs,channels=2)
-sounddevice.wait()
-write("C:/Users/asus/Desktop/Sample.wav",fs,record_voice)
-print(colorama.ansi.Fore.GREEN,"Your Record is Ready...")
-playsound("C:/Users/asus/Desktop/Sample.wav")
-print(colorama.ansi.Fore.RESET)
+while(1):
+ second=float(input("\033[1mEnter the time Duration in second: ")) #Enter your required time
+ name=input("\033[1mWhat is Name Of Voice File : ")
+ print(colorama.ansi.Fore.LIGHTRED_EX,fontstyle.apply("Recording...\n","BOLD/UNDERLINE"))
+ record_voice=sounddevice.rec(int(second*fs),fs,channels=2)
+ sounddevice.wait()
+ write(f"C:/Users/asus/Desktop/{name}.wav",fs,record_voice)
+ print(colorama.ansi.Fore.GREEN,fontstyle.apply("Your Record is Ready...","BOLD/UNDERLINE"))
+ print(colorama.ansi.Fore.CYAN)
+ 
+ while(1):
+  ch_1=input("Would You Like To Listen Your Record Of Voice (Y/N) ? : ")
+  if (ch_1=='Y' or ch_1=='y'):
+     playsound(f"C:/Users/asus/Desktop/{name}.wav")
+     break
+  elif (ch_1=='N' or ch_1=='n'):
+      break
+  else:
+      print(fontstyle.apply("\nWrong Character...\nPlease Write Again\n","RED/BOLD/INVERSE"))
+      
+ while(1):
+    print(colorama.ansi.Fore.YELLOW)
+    ch=input("Would You Like To Contunie (Y/N) ? : ")
+    if(ch=='Y' or ch=='y'):
+        time.sleep(2)
+        print(colorama.ansi.Fore.RESET)
+        break
+    elif (ch=='N' or ch=='n'):
+        print(colorama.ansi.Fore.LIGHTMAGENTA_EX,"\n\033[1mHave a Good Days...",colorama.ansi.Fore.RESET)
+        sys.exit()
+    else:
+        print(fontstyle.apply("\nWrong Character...\nPlease Write Again","RED/BOLD/INVERSE"))
+
+
