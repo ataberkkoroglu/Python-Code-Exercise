47d97d7c8b7cc4d4f2f4ee7e3670366e435bd59f
Ataberk <97685934+ataberkkoroglu@users.noreply.github.com>
2023-05-23 15:21:31 +0300

    Finding Passwords Of Wifi
    Finding Passwords Of Wifi Which was Saved to Your Computer

--- Find Password Of Wifi.py
+++ Find Password Of Wifi.py
@@ -1,9 +1,12 @@
 import subprocess
+
 x=subprocess.check_output(["netsh","wlan","show","profiles"])
 x=x.splitlines()
 t=list()
+
 for i in x:
     t.append(i.decode().split(":"))
+    
 while(1):
     try:
       t.remove([''])
@@ -13,7 +16,8 @@ name=list()
 for i in range(6,len(t)):
     name.append(t[i][1][1:])
 
-print("\n\033[1;32m--------Saved Wifi----------\n")    
+print("\n\033[1;32m--------Saved Wifi----------\n") 
+file=open("Saved_Wifi.txt","r+",encoding="UTF-8")   
 for Name in name:
     try:
         password=subprocess.check_output(["netsh","wlan","show","profile",f"{Name}","key=clear"])
@@ -21,7 +25,11 @@ for Name in name:
         password=password.split("\n")
         for i in password:
             if "Key Content" in i:
+                file.write(Name+" : "+i[29:])
                 print(f"\033[1;36m{Name} : \033[1;31m{i[29:]}")
     except:
         break
+
+file.close()
+
 print("\033[0m")
